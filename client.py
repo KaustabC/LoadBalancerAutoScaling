0593dd8fd72b464f968d9c2c1e5c02f401a6176d
coscotuff <74728041+coscotuff@users.noreply.github.com>
2023-11-07 19:20:55 +0530

    Update client.py

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 IP_addr_intermediate = "localhost"
-port_intermediate = "50050"
+port_intermediate = "60060"
 
 def run():
     print("Select from the following services: ")
@@ -24,49 +24,60 @@ def run():
     print("5. Currency conversion")
     function = int(input("Enter service number: "))
     logger.debug("Client is requesting function: " + str(function))
+  
     if function == 0:
         data1 = int(input("Enter value to be echoed: "))
         logger.debug("Sending values: " + str(data1))
+      
         with grpc.insecure_channel(IP_addr_intermediate + ":" + port_intermediate) as channel:
             stub = trial_1_pb2_grpc.AlertStub(channel)
             response = stub.InvokeMethod(trial_1_pb2.function_message(data1=data1, function=function))
             logger.debug("Echoed value: " + str(response.val))
             print("Echoed value: " + str(response.val))
+          
     elif function == 1:
         data1 = int(input("Enter principal value: "))
         data2 = int(input("Enter tenure (in years): "))
         logger.debug("Sending values: " + str(data1) + ", " + str(data2))
+      
         with grpc.insecure_channel(IP_addr_intermediate + ":" + port_intermediate) as channel:
             stub = trial_1_pb2_grpc.AlertStub(channel)
             response = stub.InvokeMethod(trial_1_pb2.function_message(data1=data1, data2=data2, function=function))
             logger.debug("Simple interest: " + str(response.val))
             print("Calculated simple interest value: " + str(response.val))
+    
     elif function == 2:
         data1 = int(input("Enter annual income: "))
         logger.debug("Sending values: " + str(data1))
+      
         with grpc.insecure_channel(IP_addr_intermediate + ":" + port_intermediate) as channel:
             stub = trial_1_pb2_grpc.AlertStub(channel)
             response = stub.InvokeMethod(trial_1_pb2.function_message(data1=data1, function=function))
             logger.debug("Tax: " + str(response.val))
             print("Computed tax value: " + str(response.val))
+          
     elif function == 3:
         data1 = int(input("Enter loan amount: "))
         data2 = int(input("Enter tenure (in years): "))
         logger.debug("Sending values: " + str(data1) + ", " + str(data2))
+      
         with grpc.insecure_channel(IP_addr_intermediate + ":" + port_intermediate) as channel:
             stub = trial_1_pb2_grpc.AlertStub(channel)
             response = stub.InvokeMethod(trial_1_pb2.function_message(data1=data1, data2=data2, function=function))
             logger.debug("EMI calculated: " + str(response.val))
             print("Computed EMI value:: " + str(response.val))
+          
     elif function == 4:
         data1 = int(input("Enter FD amount: "))
         data2 = int(input("Enter tenure (in years): "))
         logger.debug("Sending values: " + str(data1) + ", " + str(data2))
+      
         with grpc.insecure_channel(IP_addr_intermediate + ":" + port_intermediate) as channel:
             stub = trial_1_pb2_grpc.AlertStub(channel)
             response = stub.InvokeMethod(trial_1_pb2.function_message(data1=data1, data2=data2, function=function))
             logger.debug("Estimated returns: " + str(response.val))
             print("Estimated returns on FD: " + str(response.val))
+          
     elif function == 5:
         data1 = int(input("Enter money (in INR): "))
         print("The follwoing target currencies are available: ")
@@ -76,6 +87,7 @@ def run():
         print("4. JPY")
         data2 = int(input("Enter your selection: "))
         logger.debug("Sending values: " + str(data1) + ", " + str(data2))
+      
         with grpc.insecure_channel(IP_addr_intermediate + ":" + port_intermediate) as channel:
             stub = trial_1_pb2_grpc.AlertStub(channel)
             response = stub.InvokeMethod(trial_1_pb2.function_message(data1=data1, data2=data2, function=function))
